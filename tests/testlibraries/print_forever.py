e4c774101cf21fb740bb0a70521ee0885d8c53f4
Yukihiko Shinoda <yuk.hik.future@gmail.com>
2021-08-15 18:10:14 +0900

    Update test for Windows

--- tests/testlibraries/print_forever.py
+++ tests/testlibraries/print_forever.py
@@ -7,13 +7,13 @@ def main() -> None:
     count = 0
     while True:
         count = count + 1
-        if count % 2 == 0:
+        if count % 1 == 0:
             print("stdout")
         # On Windows with Python 3.9, it seems to block the output of stdout
         # when same amount of stderr is output.
-        if count % 3 == 0:
+        if count % 2 == 0:
             print("stderr", file=sys.stderr)
-        if count >= 6:
+        if count >= 2:
             count = 0
 
 
